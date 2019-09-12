b44c76a8b27e847ea1a00ee6ad5047c700eed0f2
alenaksu <misterroot@gmail.com>
2019-09-12 16:12:12 +0200

    chore(build): add external dependency to bundle and minify

--- rollup.config.js
+++ rollup.config.js
@@ -1,5 +1,6 @@
 import resolve from 'rollup-plugin-node-resolve';
 import babel from 'rollup-plugin-babel';
+import { terser } from 'rollup-plugin-terser';
 
 export default {
     // If using any exports from a symlinked project, uncomment the following:
@@ -11,9 +12,14 @@ export default {
         sourcemap: true
     },
     plugins: [
-        resolve,
         babel({
             exclude: 'node_modules/**'
-        })
+        }),
+        resolve({
+            // use "jsnext:main" if possible
+            // see https://github.com/rollup/rollup/wiki/jsnext:main
+            jsnext: true
+        }),
+        terser()
     ]
 };
